04645e527474d173a96ebe9974ffc1fbca44d813
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-01 13:43:05 +0700

    Create route & handler for adding book

---
+++ src/handler.js
@@ -0,0 +1,80 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  if (!name) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. Mohon isi nama buku',
+      })
+      .code(400);
+  }
+
+  if (readPage > pageCount) {
+    return h
+      .response({
+        status: 'fail',
+        message:
+          'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+      })
+      .code(400);
+  }
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  // Tests whether at least one element in the array passes the provided function
+  const isSuccess = books.some((book) => book.id === id);
+
+  if (isSuccess) {
+    return h
+      .response({
+        status: 'success',
+        message: 'Buku berhasil ditambahkan',
+        data: {
+          bookId: id,
+        },
+      })
+      .code(201);
+  }
+
+  return h
+    .response({
+      status: 'fail',
+      message: 'Gagal menambah buku',
+    })
+    .code(400);
+};
+
+module.exports = { addBookHandler };
