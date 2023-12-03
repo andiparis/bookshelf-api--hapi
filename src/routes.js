d8a4608b1e4ae931512338b57ac52599a54c9290
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-03 09:18:33 +0700

    Add route for getting book by id

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
-const { addBookHandler, getAllBooksHandler } = require('./handler');
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
@@ -10,6 +14,11 @@ const routes = [
     method: 'GET',
     path: '/books',
     handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
   },
 ];
 
